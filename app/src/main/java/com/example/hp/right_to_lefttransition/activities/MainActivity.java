2c97c714be4e6c5fdc73b1fa49bc6a61dfc554f3
HP-HP\HP <akm.agkgmyat@gmail.com>
2017-12-01 02:53:24 +0630

    Transition Done!

--- app/src/main/java/com/example/hp/right_to_lefttransition/activities/MainActivity.java
+++ app/src/main/java/com/example/hp/right_to_lefttransition/activities/MainActivity.java
@@ -1,15 +1,28 @@
-package com.example.hp.right_to_lefttransition;
+package com.example.hp.right_to_lefttransition.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.hp.right_to_lefttransition.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    Animation ScaleDownAndTranslate;
+    Animation ScaleUpAndTranslate;
+    CoordinatorLayout coordinatorLayout;
+    private boolean rightPanelOpen = false;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        coordinatorLayout = findViewById(R.id.coordinator_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ScaleDownAndTranslate = AnimationUtils.loadAnimation(this, R.anim.scale_down_and_translate);
+        ScaleUpAndTranslate = AnimationUtils.loadAnimation(this, R.anim.scale_up_and_translate);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -43,10 +61,20 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_profile) {
+            coordinatorLayout.startAnimation(ScaleDownAndTranslate);
+            drawerLayout.openDrawer(GravityCompat.END);
+            rightPanelOpen = true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (rightPanelOpen == true){
+            coordinatorLayout.startAnimation(ScaleUpAndTranslate);
+            drawerLayout.closeDrawer(GravityCompat.END);
+        }
     }
 }
---
+++ app/src/main/java/com/example/hp/right_to_lefttransition/components/UserProfileViewPod.java
@@ -0,0 +1,23 @@
+package com.example.hp.right_to_lefttransition.components;
+
+import android.content.Context;
+import android.support.design.widget.NavigationView;
+import android.util.AttributeSet;
+
+/**
+ * Created by HP on 11/30/2017.
+ */
+
+public class UserProfileViewPod extends NavigationView {
+    public UserProfileViewPod(Context context) {
+        super(context);
+    }
+
+    public UserProfileViewPod(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public UserProfileViewPod(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
